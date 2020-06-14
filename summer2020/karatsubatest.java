d28df6638bd4d359fe32a8deff7212575694757c
ando-huang <ahuang09182000@gmail.com>
2020-06-13 19:14:32 -0700

    solved

--- summer2020/karatsubatest.java
+++ summer2020/karatsubatest.java
@@ -1,99 +1,97 @@
 package summer2020;
 
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
+
 public class karatsubatest {
 
-    private static Long mod(Long x){
-        Long mod = Long.valueOf(1000000009);
-        if(x>= mod){
+    private static long mod(long x){
+        long mod = (long)1000000009;
+        if(x >= mod){
             return x%mod;
         }
         return (x+mod)%mod;
     }
 
-    private static Long[] arrVal(List<Long> a){
-        Long[] aR = new Long[a.size()];
+    public static long[] arrVal(List<Long> a){
+        long[] aR = new long[a.size()];
         int i = 0;
-        for(Long c: a){
-            aR[i] = c;
+        for(long c: a){
+            aR[i] = mod(c);
             i++;
         }
         return aR;
     }
 
-    private static List<Long> gradeSchool(List<Long> a, List<Long> b){
-        List<Long> result = new ArrayList<Long>();
-        Long[] aR = arrVal(a);
-        Long[] bR = arrVal(b);
-        Long[] cR = new Long[aR.length*2-1];
-        for(int i = 0; i<cR.length; i++){
-            cR[i] = Long.valueOf(0);
-        }
-        for(int i = 0; i<aR.length; i++){
-            for(int j = 0; j<bR.length; j++){
-                cR[i+j] += mod(aR[i]*bR[j]);
+    private static long[] gradeSchool(long[] a, long[] b){
+        long[] cR = new long[a.length*2-1];
+        Arrays.fill(cR, (long)0);
+        for(int i = 0; i<a.length; i++){
+            for(int j = 0; j<b.length; j++){
+                cR[i+j] += mod(mod(a[i])*mod(b[j]));
             }
         }
-        result = Arrays.asList(cR);
-        return result;
+        return cR;
     }
 
-    public static List<Long> smellCosmos(List<Long> a, List<Long> b){
-        int n = a.size();
-        Long[] aR = arrVal(a);
-        Long[] bR = arrVal(b);
-        Long[] aH = new Long[aR.length/2];
-        Long[] aL = new Long[aR.length/2];
-        Long[] aM = new Long[aR.length/2];
-        Long[] bH = new Long[bR.length/2];
-        Long[] bL = new Long[bR.length/2];
-        Long[] bM = new Long[bR.length/2];
-
-        //populate the six half arrs
-        for(int i= 0; i< aH.length; i++){
-            aH[i] = aR[i+aH.length];
-            bH[i] = bR[i+bH.length];
-            aL[i] = aR[i];
-            bL[i] = bR[i];
-            aM[i] = aH[i]+aL[i];
-            bM[i] = bH[i]+bL[i];
+    public static long[] smellCosmos(long[] a, long[] b){
+        int n = a.length;
+        if(n< 32){
+            return gradeSchool(a, b);
         }
+        long[] aH = Arrays.copyOfRange(a, a.length/2, a.length);
+        long[] aL = Arrays.copyOfRange(a, 0, a.length/2);
+        long[] bH = Arrays.copyOfRange(b, b.length/2, b.length);
+        long[] bL = Arrays.copyOfRange(b, 0, b.length/2);
+        long[] hH = smellCosmos(aH, bH); //recur on highs
+        long[] hL= smellCosmos(aL, bL);  //recur on lows
 
-        Long[] hH = arrVal(smellCosmos(Arrays.asList(aH), Arrays.asList(bH)));
-        Long[] hL= arrVal(smellCosmos(Arrays.asList(aL), Arrays.asList(bL)));
-        Long[] hM = arrVal(smellCosmos(Arrays.asList(aM), Arrays.asList(bM)));
+        long[] bM = new long[b.length/2];
+        long[] aM = new long[a.length/2];
+        //populate mid arrs
+        for(int i= 0; i< aH.length; i++){            
+            aM[i] = mod(mod(aH[i])+mod(aL[i]));
+            bM[i] = mod(mod(bH[i])+mod(bL[i]));
+        }
+        long[] hM = smellCosmos(aM, bM); //recur on middle arrs
 
         for(int i = 0; i < hM.length; i++){
-            hM[i] -= (hH[i]+ hL[i]);
+            long temp = mod(hH[i]) + mod(hL[i]);
+            hM[i] -= mod(temp);
         }
 
-        int cLen = a.size()*2-1;
-        Long[] hC = new Long[cLen];
-        for(int i = 0; i < cLen; i++){
-            hC[i] = Long.valueOf(0);
-        }
+        int cLen = n*2-1;
+        long[] hC = new long[cLen];
+        Arrays.fill(hC, (long)0);
+        
         for(int i = 0; i < hH.length; i++){
-            hC[i+n] += hH[i];
-            hC[i] += hL[i];
-            hC[i+n/2] += hH[i];
+            hC[i+n] = mod(mod(hH[i])+ mod(hC[i+n]));
+            hC[i] = mod(mod(hL[i]) + mod(hC[i]));
+            hC[i+n/2] = mod(mod(hM[i]) + mod(hC[i+n/2]));
         }
-        return Arrays.asList(hC);
+        return hC;
     }
 
     public static void main(String[] args){
-        System.out.println("input");
+        //List<Long> a = new ArrayList<Long>();
+        //List<Long> b = new ArrayList<Long>();
     }
 
 
 }
+
+/**
+ * 
+ *      
+ * 
+ *      import org.apache.commons.lang3.ArrayUtils;
+ * 
+        long[] aR = Result.arrVal(a);
+        long[] bR = Result.arrVal(b);
+        long[] res = Result.smellCosmos(aR, bR);
+        
+        Long[] resL = ArrayUtils.toObject(res);
+        List<Long> result = Arrays.asList(resL);
+
+**/
